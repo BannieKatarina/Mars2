e8f4c7470f7d9cc608e215e44baca58ded4e366c
BannieKatarina <katarina.burmistrova@yandex.ru>
2022-02-15 17:32:37 +0400

    List of profetion

--- main.py
+++ main.py
@@ -14,5 +14,10 @@ def training(prof):
     return render_template('training.html', title=f'127.0.0.1:8080/training/{prof}', proffetion=prof)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    return render_template('list.html', title=f'127.0.0.1:8080/list_prof/{list}', l=list)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
